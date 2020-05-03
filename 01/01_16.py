67b7bfde1aea78b92c9a53814f8e184b091ce521
erregung <kingelin@protonmail.com>
2020-05-03 18:45:12 +0300

    Tee vielä sekuntilaskuri

---
+++ 01/01_16.py
@@ -0,0 +1,4 @@
+# Kysytään käyttäjältä vuorokausien määrä ja lasketaan sitten niissä olevat sekunnit
+
+vuorokaudet = input("Kuinka monen vuorokauden sekunnit tulostetaan?")
+print(int(vuorokaudet) * 24 * 60 * 60)
